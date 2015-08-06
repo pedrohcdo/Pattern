7ea5e3ec982f959684c9c20442e224019dd35d3b
Pedro <pedrohcd@hotmail.com>
2015-08-05 22:05:22 -0300

    Preparations

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import processing.core.PApplet;
+import GeneticAlgorithm.AEvo;
+import GeneticAlgorithm.SimpleGenoma;
+import NeuralNetwork.MLP;
+import NeuralNetwork.TransferFunction;
+import ObstacleRace.GCar;
+import ObstacleRace.ObstacleRace;
+
+/**
+ * 
+ * @author user
+ *
+ */
+public class Main extends PApplet {
+	
+
+	/**
+	 * Setup
+	 */
+	public void setup() {
+		size(640, 480);
+		
+	}
+
+	/**
+	 * Draw
+	 */
+	public void draw() {
+		background(0);
+
+		
+	}
+}
